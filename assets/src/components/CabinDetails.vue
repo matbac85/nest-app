c6d723e25b876f96682802d03fe96d92d9aee436
matbac85 <mathildebaclin@gmail.com>
2024-05-21 17:11:45 +0200

    fixed border radius of the image on the cabin details component

--- assets/src/components/CabinDetails.vue
+++ assets/src/components/CabinDetails.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="bg-primary_200 px-6 py-8 rounded-xl drop-shadow">
-    <img :src="src" alt="" class="block min-w-full" />
-    <h2>{{ name }}</h2>
+  <div class="bg-primary_200 rounded-xl drop-shadow">
+    <div class="rounded-t-xl">
+      <img :src="src" alt="" class="block min-w-full rounded-t-xl" />
+    </div>
+    <div class="px-6 py-8">
+      <h2>{{ name }}</h2>
+    </div>
   </div>
 </template>
 
