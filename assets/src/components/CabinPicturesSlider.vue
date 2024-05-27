094582bb58009e1aa265394f70a9ec0c5c52714d
matbac85 <mathildebaclin@gmail.com>
2024-05-27 12:11:41 +0200

    did image displayer on mobile and on desktop

---
+++ assets/src/components/CabinPicturesSlider.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <!-- mobile -->
+    <div class="relative min-w-screen h-60 overflow-hidden md:hidden">
+      <img
+        :src="pictures.at(imageIndex)"
+        alt=""
+        class="min-w-full min-h-full object-cover"
+      />
+      <button
+        class="absolute p-1 rounded-full bg-primary_200 flex items-center justify-center top-1/2 transform -translate-y-1/2 right-2 opacity-80 transition ease-in-out delay-150 hover:scale-110 hover:drop-shadow hover:bg-white hover:opacity-100 duration-300"
+        @click.prevent="slideRight(pictures)"
+      >
+        <img src="../assets/arrow-right.svg" alt="" />
+      </button>
+      <button
+        class="absolute p-1 rounded-full bg-primary_200 flex items-center justify-center top-1/2 transform -translate-y-1/2 left-2 opacity-80 transition ease-in-out delay-150 hover:scale-110 hover:drop-shadow hover:bg-white hover:opacity-100 duration-300"
+        @click.prevent="slideLeft(pictures)"
+      >
+        <img src="../assets/arrow-left.svg" alt="" />
+      </button>
+    </div>
+
+    <!-- desktop -->
+
+    <div
+      class="hidden md:rouded-xl md:grid md:grid-cols-4 md:grid-rows-2 md:gap-2 md:max-h-[500px]"
+    >
+      <img
+        v-for="(picture, index) in pictures"
+        :key="index"
+        :src="picture"
+        alt=""
+        :class="getClass(index, pictures)"
+        class="w-full h-full object-cover"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+  pictures: Array,
+});
+
+const imageIndex = ref(0);
+
+const slideRight = (pictures) => {
+  imageIndex.value = (imageIndex.value + 1) % pictures.length;
+};
+
+const slideLeft = (pictures) => {
+  imageIndex.value = (imageIndex.value - 1) % pictures.length;
+};
+
+const getClass = (index, pictures) => {
+  if (index === 0) {
+    return `md:col-span-2 md:row-span-2 md:rounded-l-xl`;
+  }
+
+  if (index === 2) {
+    return `md:rounded-tr-xl`;
+  }
+
+  if (index === 4) {
+    return `md:rounded-br-xl`;
+  }
+};
+</script>
+
+<style scoped></style>
--- assets/src/pages/CabinDetailsPage.vue
+++ assets/src/pages/CabinDetailsPage.vue
@@ -2,9 +2,15 @@
   <Layout>
     <template #content>
       <main
-        class="bg-primary_300 flex-1 flex flex-col justify-center items-center"
+        class="bg-primary_300 flex-1 flex flex-col items-start justify-start md:p-20"
       >
-        <h1>{{ cabin.name }}</h1>
+        <CabinPicturesSlider :pictures="cabin.images" class="mb-6" />
+        <h1
+          class="font-dosis text-2xl font-semibold text-start text-primary_700 md:text-3xl"
+        >
+          {{ cabin.name }}
+        </h1>
+        <p>{{ cabin.description }}</p>
       </main>
     </template>
   </Layout>
@@ -14,6 +20,7 @@
 import Layout from "../components/Layout.vue";
 import { useRoute } from "vue-router";
 import { watch, ref } from "vue";
+import CabinPicturesSlider from "../components/CabinPicturesSlider.vue";
 
 const route = useRoute();
 const cabin = ref({});
