77be753d40d7433cacda710274d06ab6a5f8468d
matbac85 <mathildebaclin@gmail.com>
2024-06-16 11:15:12 +0200

    created profile card

--- assets/src/components/ProfileCard.vue
+++ assets/src/components/ProfileCard.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>{{ props.userData }}</div>
+  <div
+    class="bg-primary_200 rounded-xl drop-shadow px-6 py-8 flex gap-4 items-center"
+  >
+    <p class="bg-primary_500 rounded-full w-fit h-fit p-2">
+      {{ props.userData.firstname[0] }}{{ props.userData.lastname[0] }}
+    </p>
+    <div>
+      <h2 class="text-lg font-medium text-primary_800">
+        {{ props.userData.firstname }} {{ props.userData.lastname }}
+      </h2>
+      <p class="text-sm font-normal text-primary_800">
+        {{ props.userData.email }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup>
