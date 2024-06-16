96f08b2f66a8c7390d098ab9b17fb586cdd91c4e
matbac85 <mathildebaclin@gmail.com>
2024-06-16 10:24:44 +0200

    added a fetch data on profile page to get back all the user info and build the page

--- assets/src/pages/ProfilePage.vue
+++ assets/src/pages/ProfilePage.vue
@@ -2,6 +2,30 @@
   <div>trololo</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { watch } from "vue";
+import { userStore } from "../stores/userStore";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const fetchData = async () => {
+  const response = await fetch(`/api/me`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      authorization: `Bearer ${userStore.user.jwt}`,
+    },
+    method: "GET",
+  });
+  if (response.status === 200) {
+    console.log(await response.json());
+  } else {
+    console.log(`réservation ratée, marraine!!`);
+  }
+};
+
+watch(() => route.query, fetchData, { immediate: true });
+</script>
 
 <style lang="scss" scoped></style>
