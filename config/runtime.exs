e15d1448c12a895c4ae42e2749c6043febc519d8
ChristopheBelpaire <christophe.belpaire@selinko.com>
2024-06-18 17:10:23 +0200

    Enable https.

--- config/runtime.exs
+++ config/runtime.exs
@@ -55,14 +55,15 @@ if config_env() == :prod do
 
   config :nest, NestWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
+   # http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    #  ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    #  port: port
+    #],
+    https: [port: 443, thousand_island_options: [transport_options: [sni_fun: &CertMagex.sni_fun/1]]],
     secret_key_base: secret_key_base
 
   # ## SSL Support
