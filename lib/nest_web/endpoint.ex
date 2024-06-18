73701113efcfbb774ecb75861b9ae9d69b5f9f2e
ChristopheBelpaire <christophe.belpaire@selinko.com>
2024-06-18 23:32:29 +0200

    SiteEncrypt test

--- lib/nest_web/endpoint.ex
+++ lib/nest_web/endpoint.ex
@@ -1,5 +1,35 @@
 defmodule NestWeb.Endpoint do
-  use Phoenix.Endpoint, otp_app: :nest
+  use SiteEncrypt.Phoenix.Endpoint, otp_app: :nest
+
+
+  def certification() do
+    SiteEncrypt.configure(
+      # Note that native client is very immature. If you want a more stable behaviour, you can
+      # provide `:certbot` instead. Note that in this case certbot needs to be installed on the
+      # host machine.
+      client: :native,
+
+      domains: ["nest.selinko.be"],
+      emails: ["christophe.belpaire@gmail.com"],
+
+      # By default the certs will be stored in tmp/site_encrypt_db, which is convenient for
+      # local development. Make sure that tmp folder is gitignored.
+      #
+      # Set OS env var SITE_ENCRYPT_DB on staging/production hosts to some absolute path
+      # outside of the deployment folder. Otherwise, the deploy may delete the db_folder,
+      # which will effectively remove the generated key and certificate files.
+      db_folder:
+        System.get_env("SITE_ENCRYPT_DB", Path.join("tmp", "site_encrypt_db")),
+
+      # set OS env var CERT_MODE to "staging" or "production" on staging/production hosts
+      directory_url:
+        case System.get_env("CERT_MODE", "local") do
+          "local" -> {:internal, port: 4002}
+          "staging" -> "https://acme-staging-v02.api.letsencrypt.org/directory"
+          "production" -> "https://acme-v02.api.letsencrypt.org/directory"
+        end
+    )
+  end
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
